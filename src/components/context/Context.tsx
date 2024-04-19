fc6dbdeb4a22d05d6adef2e1c889e8e147aac76b
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-19 16:34:32 +0200

    :sparkles: adds context for smaller/bigger screen sizing

---
+++ src/components/context/Context.tsx
@@ -0,0 +1,42 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+const Context = createContext<IProviderTypes | undefined>(undefined);
+
+interface IProviderTypes {
+  isMobile: boolean;
+  isDesktop: boolean;
+}
+
+interface IContextType {
+  children: ReactNode;
+}
+
+export const ContextProvider: React.FC<IContextType> = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const resizeWindow = () => {
+      const screenSize = window.innerWidth;
+      setIsMobile(screenSize < 744);
+      setIsDesktop(screenSize >= 1024);
+    };
+
+    window.addEventListener('resize', resizeWindow);
+
+    resizeWindow();
+
+    return () => {
+      window.removeEventListener('resize', resizeWindow);
+    };
+  }, []);
+
+  const contextValue = {
+    isMobile: isMobile,
+    isDesktop: isDesktop
+  };
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
+
+export default Context;
