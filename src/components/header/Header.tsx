c82508d61ac070e6034f893a86163a208fe9b0e8
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-19 09:15:54 +0200

    :building_construction: adds components

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import LogoComponent from "./Logo";
+import Divider from "../shared/Divider";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header className="header">
+        <LogoComponent />
+        <Divider /> 
+        <Navbar /> 
+    </header>
+  )
+}
+
+export default Header; 
