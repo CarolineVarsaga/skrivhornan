10f1f21adbdff93fb11eada86c090ce106dfce92
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-19 16:35:10 +0200

    :sparkles: adds presentation and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Header from "./components/header/Header";
+import Presentation from "./components/presentation/Presentation";
+import { useContext } from 'react';
+import Context from './components/context/Context';
+
 function App() {
+  const context = useContext(Context);
+  if (!context) return;
+
   return (
     <>
       <Header />
+      <Presentation />
     </>
   )
 }
