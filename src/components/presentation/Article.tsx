699425c7fa8852ea6c378c2d721b71bf4cd41f1a
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-19 16:33:47 +0200

    :sparkles: adds article component

---
+++ src/components/presentation/Article.tsx
@@ -0,0 +1,20 @@
+import Selfportrait from '../../assets/images/selfportrait.jpg';
+import Divider from '../shared/Divider';
+
+const Article = () => {
+  return (
+    <article className='presentation-article'>
+        <img className="photo" src={Selfportrait} alt="profilbild av Torbjörn Vårsaga" width="489" height="521" />
+        <h1>Kommunikatör</h1>
+        <Divider />
+        <p className='presentation-paragraph'>
+            Ord som träffar rakt i hjärtat på dig kan falla 
+            platt inför den målgrupp du vill nå. När du 
+            behöver en erfaren brobyggare står jag redo 
+            att hjälpa dig.
+        </p>
+    </article>
+  );
+};
+
+export default Article;
