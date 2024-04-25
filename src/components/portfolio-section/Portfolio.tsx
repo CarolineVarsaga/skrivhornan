49c390d3a3f50b017fadeccba406a7d49614cdad
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-25 16:00:32 +0200

    :building_construction: adds portfolio section

---
+++ src/components/portfolio-section/Portfolio.tsx
@@ -0,0 +1,15 @@
+
+const Portfolio = () => {
+  return (
+    <section className="portfolio-section">
+        <div className="portfolio-background">
+            <h2 className="portfolio-heading">Portfolio</h2>
+            <div className="portfolio-card"></div>
+            <button className="portfolio-button">Fler uppdrag</button>
+        </div>
+        
+    </section>
+  )
+}
+
+export default Portfolio
