b0302cc619261b74b84cc054f0d55cbdf10c6d20
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-26 14:15:58 +0200

    :building_construction: adds cardData array

--- src/components/portfolio-section/Portfolio.tsx
+++ src/components/portfolio-section/Portfolio.tsx
@@ -1,15 +1,35 @@
+import Card from "./Card"
+import cardImage from "../../assets/images/selfportrait.jpg"
+const Portfolio: React.FC = () => {
 
-const Portfolio = () => {
+    const cardData = [
+        {
+            image: cardImage, 
+            title: 'Titel1',
+            description: 'En kort text om projektet. Bla bla bla, max två rader kanske. Här ryms det kanske ganska mycket. Ryms det ännu mer? '
+        },
+        {
+            image: '', 
+            title: 'Titel2',
+            description: 'En kort text om projektet. Bla bla bla, max två rader kanske. '
+        },
+        {
+            image: '', 
+            title: 'Titel3',
+            description: 'En kort text om projektet. Bla bla bla, max två rader kanske. '
+        },
+    ]
+
   return (
     <section className="portfolio-section">
         <div className="portfolio-background">
             <h2 className="portfolio-heading">Portfolio</h2>
-            <div className="portfolio-card"></div>
+            <Card data={cardData} />
             <button className="portfolio-button">Fler uppdrag</button>
         </div>
         
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
