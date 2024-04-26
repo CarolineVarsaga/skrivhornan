66a4fa40ffb29d09ba26f6b7d6d0af05fa993d84
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-04-26 16:14:45 +0200

    :fire: removed from old folder

--- src/components/header/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Logo from '../../assets/svg/skrivhornan-logo.svg';
-
-const LogoComponent = () => {
-  return (
-    <img className="logo" src={Logo} alt="Logotyp: Skrivhörnan Torbjörn Vårsaga" width="592" height="100" />
-  );
-};
-
-export default LogoComponent;
